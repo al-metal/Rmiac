97f8fd2dd9240022509b3dee99b4b812a126e631
al-metal <al-metal@bk.ru>
2017-03-06 22:55:42 +0400

    Добавление записей через форму

--- Rmiac/NoteBookEdits.Designer.cs
+++ Rmiac/NoteBookEdits.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.tbTheme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPhone = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(402, 271);
+            this.button1.Location = new System.Drawing.Point(402, 186);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,22 +49,103 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tbTheme
+            // 
+            this.tbTheme.Location = new System.Drawing.Point(53, 12);
+            this.tbTheme.Name = "tbTheme";
+            this.tbTheme.Size = new System.Drawing.Size(196, 20);
+            this.tbTheme.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Тема";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Тема";
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(307, 12);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Текст";
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(53, 41);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(196, 133);
+            this.rtbText.TabIndex = 7;
+            this.rtbText.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Номер";
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(53, 180);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(196, 20);
+            this.tbPhone.TabIndex = 8;
+            // 
             // NoteBookEdits
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 306);
+            this.ClientSize = new System.Drawing.Size(489, 218);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calendar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTheme);
             this.Controls.Add(this.button1);
             this.Name = "NoteBookEdits";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Новая запись";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NoteBookEdits_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbTheme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPhone;
     }
 }
--- Rmiac/NoteBookEdits.cs
+++ Rmiac/NoteBookEdits.cs
@@ -26,63 +26,68 @@ namespace Rmiac
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connStr = "Data Source=localhost;Initial Catalog=DB;Integrated Security=True";
-
-            SqlConnection conn = new SqlConnection(connStr);
-            try
+            string date = calendar.SelectionEnd.Date.ToString();
+            date = date.Substring(date.IndexOf(' '));
+            if(tbTheme.Text != "" && rtbText.Text != "")
             {
-                conn.Open();
-            }
-            catch (SqlException se)
-            {
-                MessageBox.Show("Ошибка подключения:{0}", se.Message);
-                return;
-            }
+                string connStr = "Data Source=localhost;Initial Catalog=DB;Integrated Security=True";
 
-            SqlCommand cmd = new SqlCommand(@"INSERT INTO
+                SqlConnection conn = new SqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+                }
+                catch (SqlException se)
+                {
+                    MessageBox.Show("Ошибка подключения:{0}", se.Message);
+                    return;
+                }
+
+                SqlCommand cmd = new SqlCommand(@"INSERT INTO
                                                 NoteBook ([theme], [text], [date], [phone])
                                              VALUES
                                             (@theme, @text, @date, @phone)", conn);
 
-            SqlParameter param = new SqlParameter();
-            param.ParameterName = "@theme";
-            param.Value = "Новая провер";
-            param.SqlDbType = SqlDbType.NVarChar;
-            cmd.Parameters.Add(param);
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@theme";
+                param.Value = "Новая провер";
+                param.SqlDbType = SqlDbType.NVarChar;
+                cmd.Parameters.Add(param);
 
-            param = new SqlParameter();
-            param.ParameterName = "@text";
-            param.Value = "Иванов И";
-            param.SqlDbType = SqlDbType.NVarChar;
-            cmd.Parameters.Add(param);
+                param = new SqlParameter();
+                param.ParameterName = "@text";
+                param.Value = "Иванов И";
+                param.SqlDbType = SqlDbType.NVarChar;
+                cmd.Parameters.Add(param);
 
-            param = new SqlParameter();
-            param.ParameterName = "@date";
-            param.Value = "06.03.2017";
-            param.SqlDbType = SqlDbType.Date;
-            cmd.Parameters.Add(param);
+                param = new SqlParameter();
+                param.ParameterName = "@date";
+                param.Value = "06.03.2017";
+                param.SqlDbType = SqlDbType.Date;
+                cmd.Parameters.Add(param);
 
-            param = new SqlParameter();
-            param.ParameterName = "@phone";
-            param.Value = "123456";
-            param.SqlDbType = SqlDbType.NChar;
-            cmd.Parameters.Add(param);
+                param = new SqlParameter();
+                param.ParameterName = "@phone";
+                param.Value = "123456";
+                param.SqlDbType = SqlDbType.NChar;
+                cmd.Parameters.Add(param);
 
-            Console.WriteLine("Вставляем запись");
-            try
-            {
-                cmd.ExecuteNonQuery();
+                Console.WriteLine("Вставляем запись");
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка, при выполнении запроса на добавление записи");
+                    return;
+                }
+
+                cmd = new SqlCommand("Select * From Students", conn);
+
+                conn.Close();
+                conn.Dispose();
             }
-            catch
-            {
-                MessageBox.Show("Ошибка, при выполнении запроса на добавление записи");
-                return;
-            }
-
-            cmd = new SqlCommand("Select * From Students", conn);
-
-            conn.Close();
-            conn.Dispose();
         }
     }
 }
